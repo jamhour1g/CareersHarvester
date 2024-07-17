242d634d98d42bcebe807d248e260ae4bf89d932
jamhour1g <yazanjam50@gmail.com>
2024-07-17 13:57:57 +0300

    feat(JobPosterBusinessType): Add business types to help with categorizing job posters

---
+++ src/main/kotlin/com/jamhour/core/poster/JobPosterBusinessType.kt
@@ -0,0 +1,13 @@
+package com.jamhour.core.poster
+
+enum class JobPosterBusinessType {
+    LIMITED_LIABILITY_COMPANY,
+    SOLE_PROPRIETORSHIP,
+    COOPERATIVE,
+    CORPORATION,
+    FRANCHISE,
+    PARTNERSHIP,
+    LIMITED_PARTNERSHIP,
+    NON_PROFIT,
+    OTHER
+}
