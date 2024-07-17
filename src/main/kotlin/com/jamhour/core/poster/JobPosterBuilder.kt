65ffafde8e929bcbcc4d1bb54ee11ce761c000bf
jamhour1g <yazanjam50@gmail.com>
2024-07-17 13:52:27 +0300

    feat(JobPosterBuilder): Introduce a job poster builder to simplify the job poster creating process.

---
+++ src/main/kotlin/com/jamhour/core/poster/JobPosterBuilder.kt
@@ -0,0 +1,21 @@
+package com.jamhour.core.poster
+
+import com.jamhour.core.provider.JobsProvider
+import java.net.URI
+import java.time.LocalDate
+
+interface JobPosterBuilder {
+
+    fun name(name: String): JobPosterBuilder
+    fun location(location: String): JobPosterBuilder
+    fun website(website: URI): JobPosterBuilder
+    fun establishmentDate(establishmentDate: LocalDate): JobPosterBuilder
+    fun businessType(businessType: JobPosterBusinessType): JobPosterBuilder
+    fun contactInfo(contactInfo: JobPosterContactInfo): JobPosterBuilder
+    fun overview(overview: String): JobPosterBuilder
+    fun uriOnProvider(uriOnProvider: URI): JobPosterBuilder
+    fun provider(provider: JobsProvider): JobPosterBuilder
+    fun verification(verification: JobProviderVerification): JobPosterBuilder
+    fun build(): JobPoster
+
+}
