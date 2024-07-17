23fde3203fb9b538ff7830c1113b47d57c65fe73
jamhour1g <yazanjam50@gmail.com>
2024-07-17 13:49:06 +0300

    feat(JobBuilder): Introduce a Job builder to simplify the job-creating process.

---
+++ src/main/kotlin/com/jamhour/core/job/JobBuilder.kt
@@ -0,0 +1,30 @@
+package com.jamhour.core.job
+
+import com.jamhour.core.poster.JobPoster
+import com.jamhour.core.provider.JobsProvider
+import java.net.URI
+import java.time.LocalDate
+
+interface JobBuilder {
+
+    fun title(title: String): JobBuilder
+    fun location(location: String): JobBuilder
+    fun description(description: String): JobBuilder
+    fun responsibilities(responsibilities: String): JobBuilder
+    fun qualifications(qualifications: String): JobBuilder
+    fun preksAndBenefits(preksAndBenefits: String): JobBuilder
+    fun instructionsOnHowToApply(instructionsOnHowToApply: String): JobBuilder
+    fun typeOfVacancy(typeOfVacancy: JobVacancyType): JobBuilder
+    fun poster(poster: JobPoster): JobBuilder
+    fun requirements(requirements: String): JobBuilder
+    fun positionLevel(positionLevel: String): JobBuilder
+    fun salary(salary: String): JobBuilder
+    fun experience(experience: String): JobBuilder
+    fun degree(degree: String): JobBuilder
+    fun jobsProvider(jobsProvider: JobsProvider): JobBuilder
+    fun jobPublishDate(jobPublishDate: LocalDate): JobBuilder
+    fun jobDeadline(jobDeadline: LocalDate): JobBuilder
+    fun jobURI(jobURI: URI): JobBuilder
+    fun build(): Job
+
+}
