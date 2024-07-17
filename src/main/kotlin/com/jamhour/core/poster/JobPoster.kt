10ec682c477bc03889ac498bf0058c54ff1fb21b
jamhour1g <yazanjam50@gmail.com>
2024-07-17 13:51:31 +0300

    feat(JobPoster): Introduce an interface for representing job posters

---
+++ src/main/kotlin/com/jamhour/core/poster/JobPoster.kt
@@ -0,0 +1,23 @@
+package com.jamhour.core.poster
+
+import com.jamhour.core.job.Job
+import com.jamhour.core.provider.JobsProvider
+import java.net.URI
+import java.time.LocalDate
+
+interface JobPoster : Comparable<JobPoster> {
+
+    val posterName: String
+    val posterLocation: String
+    val posterWebsite: URI?
+    val posterEstablishmentDate: LocalDate?
+    val allJobsFromPoster: List<Job>
+    val businessType: JobPosterBusinessType
+    val posterContactInfo: JobPosterContactInfo
+    val posterOverview: String
+    val posterUriOnProvider: URI?
+    val posterProvider: JobsProvider
+    val providerVerification: JobProviderVerification
+    val jobPosterBuilder: JobPosterBuilder
+
+}
