0ac579f36fdaab6f3984956fdc8c600cdb4ead23
jamhour1g <yazanjam50@gmail.com>
2024-07-17 13:48:01 +0300

    feat(Job): Introduce an interface for representing jobs

---
+++ src/main/kotlin/com/jamhour/core/job/Job.kt
@@ -0,0 +1,32 @@
+package com.jamhour.core.job
+
+import com.jamhour.core.poster.JobPoster
+import com.jamhour.core.poster.JobPosterContactInfo
+import com.jamhour.core.provider.JobsProvider
+import java.net.URI
+import java.time.LocalDate
+
+interface Job : Comparable<Job> {
+
+    val jobTitle: String
+    val jobLocation: String
+    val jobDescription: String
+    val jobResponsibilities: String
+    val jobRequirements: String
+    val jobPositionLevel: String
+    val jobSalary: String
+    val jobExperience: String
+    val jobDegree: String
+    val jobQualifications: String
+    val jobInstructionsOnHowToApply: String
+    val jobPreksAndBenefits: String
+    val jobPublishDate: LocalDate?
+    val jobDeadline: LocalDate?
+    val typeOfVacancy: JobVacancyType
+    val jobPoster: JobPoster
+    val jobPosterContactInfo: JobPosterContactInfo
+    val jobsProvider: JobsProvider
+    val jobURI: URI
+    val jobBuilder: JobBuilder
+
+}
