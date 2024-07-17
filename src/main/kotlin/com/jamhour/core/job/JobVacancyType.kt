d741c8400c28b30128e56634c219824656175162
jamhour1g <yazanjam50@gmail.com>
2024-07-17 14:06:43 +0300

    feat(JobVacancyType): Add a vacancy type to help with categorizing job postings

---
+++ src/main/kotlin/com/jamhour/core/job/JobVacancyType.kt
@@ -0,0 +1,13 @@
+package com.jamhour.core.job
+
+enum class JobVacancyType {
+
+    FULL_TIME,
+    PART_TIME,
+    INTERNSHIP,
+    CONTRACTOR,
+    TEMPORARY,
+    VOLUNTEER,
+    NOT_SPECIFIED
+
+}
