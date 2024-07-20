c592e1fa99ece2e2165407c6324bb70c6322e6a1
jamhour1g <yazanjam50@gmail.com>
2024-07-20 19:12:59 +0300

    feat(JobPosterImpl): Implement JobPoster
    Introduces JobPosterImpl, a concrete implementation of the JobPoster
    interface. Enables representation of entities posting jobs within the
    system.

---
+++ src/main/kotlin/com/jamhour/core/poster/impl/JobPosterImpl.kt
@@ -0,0 +1,27 @@
+package com.jamhour.core.poster.impl
+
+import com.jamhour.core.job.Job
+import com.jamhour.core.poster.JobPoster
+import com.jamhour.core.poster.JobPosterBuilder
+import com.jamhour.core.poster.JobPosterBusinessType
+import com.jamhour.core.poster.JobPosterContactInfo
+import com.jamhour.core.provider.JobProviderVerification
+import com.jamhour.core.provider.JobsProvider
+import java.net.URI
+import java.time.LocalDate
+
+@JvmRecord
+data class JobPosterImpl(
+    override val posterName: String,
+    override val posterLocation: String,
+    override val posterWebsite: URI?,
+    override val posterEstablishmentDate: LocalDate?,
+    override val allJobsFromPoster: List<Job>,
+    override val businessType: JobPosterBusinessType,
+    override val posterContactInfo: JobPosterContactInfo,
+    override val posterOverview: String,
+    override val posterUriOnProvider: URI?,
+    override val posterProvider: JobsProvider,
+    override val providerVerification: JobProviderVerification,
+    override val jobPosterBuilder: JobPosterBuilder
+) : JobPoster
