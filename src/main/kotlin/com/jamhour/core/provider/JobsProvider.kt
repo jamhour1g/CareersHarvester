2906e657aaff7c6873cce8634743b4c5116301f4
jamhour1g <yazanjam50@gmail.com>
2024-07-20 23:13:30 +0300

    feat(JobsProvider): CoroutineScope, cachedJobs, getJobs suspend
    Introduces CoroutineScope for asynchronous job retrieval. Adds "val
    cachedJobs: Deferred<List<Job>>" property for potential caching.
    Adds a suspendable "getJobs()" method enabling asynchronous job
    retrieval. Enhances flexibility and potential performance with
    coroutines.

--- src/main/kotlin/com/jamhour/core/provider/JobsProvider.kt
+++ src/main/kotlin/com/jamhour/core/provider/JobsProvider.kt
@@ -1,21 +1,30 @@
 package com.jamhour.core.provider
 
 import com.jamhour.core.job.Job
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
 import java.net.URI
+import kotlin.coroutines.CoroutineContext
 
 val providerComparator: Comparator<JobsProvider> =
     Comparator.comparing<JobsProvider, String> { it.providerURI?.toString() ?: "" }
         .thenComparing(JobsProvider::providerName)
         .thenComparing(JobsProvider::providerDescription)
 
-interface JobsProvider : Comparable<JobsProvider> {
+interface JobsProvider : Comparable<JobsProvider>, CoroutineScope {
 
     val providerName: String
     val providerDescription: String
     val providerURI: URI?
     val providerStatus: JobProviderStatus
-    val providerJobs: List<Job>
+    val cachedJobs: Deferred<List<Job>>
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default + kotlinx.coroutines.Job()
 
     override fun compareTo(other: JobsProvider) = providerComparator.compare(this, other)
+
+    suspend fun getJobs(): List<Job>
 
 }
