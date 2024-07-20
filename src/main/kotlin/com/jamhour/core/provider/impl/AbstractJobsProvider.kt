d61e76cba72145999d8c814f3b2e267f44ee945d
jamhour1g <yazanjam50@gmail.com>
2024-07-20 23:20:06 +0300

    feat(AbstractJobsProvider): Introduce base class for JobProviders
    Introduces AbstractJobsProvider class as a base for concrete JobProvider
     implementations. Provides a foundation for shared functionality within
     JobProviders, promoting code reuse and maintainability.

---
+++ src/main/kotlin/com/jamhour/core/provider/impl/AbstractJobsProvider.kt
@@ -0,0 +1,30 @@
+package com.jamhour.core.provider.impl
+
+import com.jamhour.core.job.Job
+import com.jamhour.core.provider.JobProviderStatus
+import com.jamhour.core.provider.JobsProvider
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import java.net.URI
+import java.util.Objects
+
+abstract class AbstractJobsProvider(
+    override val providerName: String,
+    override val providerDescription: String,
+    override val providerURI: URI?,
+    override val providerStatus: JobProviderStatus,
+) : JobsProvider {
+    override val cachedJobs: Deferred<List<Job>> by lazy { async { getJobs() } }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is AbstractJobsProvider) return false
+        return providerName == other.providerName &&
+                providerDescription == other.providerDescription &&
+                providerURI == other.providerURI
+    }
+
+    override fun hashCode() = Objects.hash(providerName, providerDescription, providerURI)
+    override fun toString() =
+        "AbstractJobsProvider(providerName='$providerName', providerDescription='$providerDescription', providerURI=$providerURI, providerStatus=$providerStatus)"
+}
