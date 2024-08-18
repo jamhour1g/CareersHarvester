44900b02f9a2cdb1e29530b000d17da1fb458d68
jamhour1g <yazanjam50@gmail.com>
2024-08-18 06:17:56 +0300

    build(build.gradle): Add kotlinx-serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "com.jamhour"
@@ -11,7 +12,9 @@ repositories {
 
 dependencies {
     val coroutinesVersion = "1.9.0-RC"
+    val serializationVersion = "1.7.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
     testImplementation(kotlin("test"))
 }
 
