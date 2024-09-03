aec7338e134de97827ddb1e33271ab2d602f0700
jamhour1g <yazanjam50@gmail.com>
2024-09-03 23:35:22 +0300

    feat(BambooHRProviders): Remove old FoothillSolutions and Userpilot implementations
    Remove the outdated implementations of `FoothillSolutions.kt` and
    `Userpilot.kt` in favor of new versions that extend
    `AbstractBambooHRJobProvider`.
    
    The transition to `AbstractBambooHRJobProvider` ensures a more
    consistent and efficient approach to integrating with the BambooHR
    platform across these providers.

---
+++ src/main/kotlin/com/jamhour/core/provider/impl/json/BambooHRJobProviders.kt
@@ -0,0 +1,49 @@
+package com.jamhour.core.provider.impl.json
+
+import com.jamhour.util.loggerFactory
+import com.jamhour.util.toURI
+
+class Userpilot : AbstractBambooHRJobProvider(
+    NAME,
+    LOCATION,
+    providerURI,
+    careerPageLink
+) {
+    override val logger = loggerFactory(Userpilot::class.java)
+
+    override fun filterJob(job: BambooHRJob): Boolean {
+        val isInPalestine = job.location.toJobLocation() == LOCATION
+        val isInSoftware = job.departmentId == SOFTWARE_DEPARTMENT_CODE
+        return (isInSoftware && isInPalestine) || (isInSoftware && job.isRemote == true)
+    }
+
+    companion object {
+        private const val LOCATION = "West Bank,Ramallah"
+        private const val NAME = "Userpilot"
+        private const val SOFTWARE_DEPARTMENT_CODE = 18573
+        private val providerURI = "https://www.userpilot.com/".toURI()
+        private val careerPageLink = "https://userpilot.bamboohr.com/careers".toURI()
+    }
+}
+
+class Foothill : AbstractBambooHRJobProvider(
+    NAME,
+    LOCATION,
+    providerURI,
+    careerPageLink
+) {
+
+    override val logger = loggerFactory(Foothill::class.java)
+
+    override fun filterJob(job: BambooHRJob): Boolean = job.departmentId == SOFTWARE_DEPARTMENT_CODE
+
+    companion object {
+        private const val NAME = "FoothillSolutions"
+        private const val LOCATION = "Nablus"
+        private const val SOFTWARE_DEPARTMENT_CODE = 18504
+        private val providerURI = "https://www.foothillsolutions.com/".toURI()
+        private val careerPageLink = "https://foothillsolutions.bamboohr.com/careers".toURI()
+    }
+
+}
+
