1269d61af13e7af627a1418e14cea27d07ed464b
jamhour1g <yazanjam50@gmail.com>
2024-09-10 17:45:01 +0300

    build(build.gradle): Add shadowJar plugin to package JAR with dependencies
    Introduce the `shadowJar` plugin to allow for the creation of a JAR
    file with all dependencies using the `./gradlew shadowJar` command.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
+    val kotlinVersion = "2.0.20"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.jamhour"
@@ -14,18 +17,25 @@ dependencies {
     val coroutinesVersion = "1.9.0-RC.2"
     val serializationVersion = "1.7.2"
     val jsoupVersion = "1.18.1"
-    val kotlinStdLibVersion = "2.0.20"
+    val kotlinVersion = "2.0.20"
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
     implementation("org.jsoup:jsoup:$jsoupVersion") // https://mvnrepository.com/artifact/org.jsoup/jsoup
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinStdLibVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    // Create a fat JAR with dependencies
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveClassifier = "all"
+    }
 }
 
 kotlin {
